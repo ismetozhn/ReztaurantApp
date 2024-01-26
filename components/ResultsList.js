b54c57c0b5d65c34d6b8e335f69d68a2e2e8835a
ismetozhn <ismetozhn@hotmail.com>
2024-01-26 14:43:33 +0300

    restaurants listed

--- components/ResultsList.js
+++ components/ResultsList.js
@@ -1,13 +1,39 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, FlatList, TouchableOpacity } from 'react-native'
 import React from 'react'
+import ResultDetail from './ResultDetail';
 
-export default function ResultsList({title, results}) {
-    console.log(results);
-  return (
-    <View>
-      <Text>{title}</Text>
-    </View>
-  )
+
+export default function ResultsList({ title, results }) {
+   
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>{title}</Text>
+            <FlatList 
+            horizontal
+            showsHorizontalScrollIndicator={false}
+                data={results}
+                renderItem={({ item }) => {
+                    return (
+                        <TouchableOpacity>
+                            <ResultDetail result={item} />
+                        </TouchableOpacity>
+                    )
+                }}
+            />
+        </View>
+    )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container:{
+        marginBottom: 10
+    },
+    title:{
+        marginLeft: 15,
+        marginBottom:5,
+        fontSize: 18,
+        fontWeight: 'bold',
+        fontStyle:'italic'
+    }
+  
+})
